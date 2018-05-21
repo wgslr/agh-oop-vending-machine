605bff627ace0f0ab500d77cf38cc6d51952731a
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-05-21 08:02:52 +0200

    Fill machine with products.

--- src/main/agh/cs/lab/vending_machine/VendingMachineREPL.java
+++ src/main/agh/cs/lab/vending_machine/VendingMachineREPL.java
@@ -1,15 +1,31 @@
 package agh.cs.lab.vending_machine;
 
 
-import java.util.Collections;
+import java.util.*;
 
 public class VendingMachineREPL {
     static Machine machine;
+
     public static void main(String[] args) {
-        machine = new Machine(Collections.emptyList(), new CoinSlot(2));
 
-        // TODO fill product types
+        int productCount = 6;
+        List<Product> products = new ArrayList<>(productCount);
 
+        Random rng = new Random();
+        Set<Integer> generated = new LinkedHashSet<>();
+        while (generated.size() < productCount) {
+            Integer next = rng.nextInt(2137) + 1;
+            generated.add(next);
+        }
+        Iterator<Integer> iterator = generated.iterator();
+        products.add(new Product(iterator.next(), "mamba", 8.0, 8));        // 1
+        products.add(new Product(iterator.next(), "monsterek", 5.99, 6));   // 2
+        products.add(new Product(iterator.next(), "paluszkis", 4.29, 8));   // 3
+        products.add(new Product(iterator.next(), "water", 1.25, 10));      // 4
+        products.add(new Product(iterator.next(), "tissues", 0.85, 20));    // 5
+        products.add(new Product(iterator.next(), "mentoski", 2.34, 15));   // 6
+
+        machine = new Machine(products, new CoinSlot(2));
     }
 
     void repl() {
